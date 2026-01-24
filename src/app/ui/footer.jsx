133f1ac6daeba3b2924a23f604aca3d52aabb176
Hariom Kashyap <omkashyap6870@gmail.com>
2026-01-25 01:22:49 +0530

    update icons

--- src/app/ui/footer.jsx
+++ src/app/ui/footer.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import { FaInstagram, FaLinkedinIn, FaXTwitter, FaGithub, FaEnvelope } from "react-icons/fa6"
-import { Heading3 } from "../components";
+import { EmailIcon, FigmaIcon, GithubIcon, InstagramIcon, LinkedInIcon, TwitterXIcon, } from "../icons";
 
 export default function Footer() {
   return (
@@ -9,19 +8,22 @@ export default function Footer() {
         <div className="flex justify-center items-center">
           <div className="flex gap-6">
             <Link className="text-2xl text-neutral-600 social-links " target="black" href="https://www.instagram.com/omkashyap7484">
-              <FaInstagram />
+              <InstagramIcon />
             </Link>
             <Link className="text-2xl text-neutral-600 social-links" target="black" href="https://www.linkedin.com/in/hari-om-kashyap-b176b63a5">
-              <FaLinkedinIn />
+              <LinkedInIcon />
             </Link>
             <Link className="text-2xl text-neutral-600 social-links" target="black" href="https://x.com/om_kashyap28">
-              <FaXTwitter />
+              <TwitterXIcon />
             </Link>
             <Link className="text-2xl text-neutral-600 social-links" target="black" href="https://github.com/omkashyap28">
-              <FaGithub />
+              <GithubIcon />
             </Link>
             <Link className="text-2xl text-neutral-600 social-links" target="black" href="mailto:omkashyap6870@gmail.com">
-              <FaEnvelope />
+              <EmailIcon />
+            </Link>
+            <Link className="text-2xl text-neutral-600 social-links" target="black" href="https://www.figma.com/@hariom7">
+              <FigmaIcon />
             </Link>
           </div>
         </div>
