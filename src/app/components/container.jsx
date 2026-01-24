d642297323e975f34d6ed5d2b517826c0f532a4a
Hariom Kashyap <omkashyap6870@gmail.com>
2026-01-25 01:25:02 +0530

    move lenis config to container for apply  smooth scrolling to all pages

--- src/app/components/container.jsx
+++ src/app/components/container.jsx
@@ -1,7 +1,31 @@
+"use client"
+
+import gsap from 'gsap'
+import { ReactLenis } from 'lenis/react'
+import { useEffect, useRef } from 'react'
+
 export default function Container({ children }) {
+  const lenisRef = useRef();
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+    gsap.ticker.add(update)
+    return () => gsap.ticker.remove(update);
+  }, []);
   return (
-    <div className="w-full max-w-2xl mx-auto px-5">
-      {children}
-    </div>
+    <>
+      <ReactLenis
+        options={{
+          smoothWheel: true,
+          duration: 1,
+          easing: "easeOutExpo",
+          autoRef: false
+        }}
+        ref={lenisRef} root />
+      <div className="w-full max-w-2xl mx-auto px-5">
+        {children}
+      </div>
+    </>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,7 @@
-"use client"
-
-import gsap from 'gsap'
-import { ReactLenis } from 'lenis/react'
-import { useEffect, useRef } from 'react'
 import { Hero, Projects, About, Other, Cta } from "./ui";
-
-
 export default function Home() {
-  const lenisRef = useRef();
-  useEffect(() => {
-    function update(time) {
-      lenisRef.current?.lenis?.raf(time * 1000)
-    }
-    gsap.ticker.add(update)
-    return () => gsap.ticker.remove(update);
-  }, []);
-
   return (
     <>
-      <ReactLenis
-        options={{
-          smoothWheel: true,
-          duration: 1,
-          easing: "easeOutExpo",
-          autoRef: false
-        }}
-        ref={lenisRef} root />
       <Hero />
       <Projects />
       <About />
